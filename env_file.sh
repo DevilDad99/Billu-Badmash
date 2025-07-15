b3d5774beb29606cfcd4058fd354f68a964a89e9
DevilDad99 <aldammamelect@gmail.com>
2025-07-15 13:44:34 +0400

    Update env_file.sh

--- env_file.sh
+++ env_file.sh
@@ -1,5 +1,5 @@
 # Discord Bot Token
-DISCORD_TOKEN=your_bot_token_here
+DISCORD_TOKEN=MTM5NDYwOTI0NzQzNzg1MjcxNA.G4V0rZ.mhM_2CxQwwCJPbxww0U7VlaxxgNwLCQgAxmJBM
 
 # Optional: Set log level
 LOG_LEVEL=INFO
